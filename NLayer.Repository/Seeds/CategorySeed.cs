2b7c8ba6f24b243d859a975018e16768b21ed21a
Ozan Mudul <mudulozan@gmail.com>
2022-08-14 18:59:51 +0300

    Seed Data

---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,23 @@
+﻿using NLayer.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NLayer.Repository.Seeds
+{
+    public class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Kalemler" },
+                new Category { Id = 2, Name = "Kitaplar" },
+                new Category { Id = 3, Name = "Defterler" }
+            );
+        }
+    }
+}
