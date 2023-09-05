585c9dfffc0514cd3a5192c78c0c21e03ee41bd6
Ozan Mudul <mudulozan@gmail.com>
2023-09-05 20:29:21 +0300

    FluentValidation

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -11,6 +11,5 @@ public class ProductDtoValidator : AbstractValidator<ProductDto>
 
         RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
         RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
-        RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
     }
 }
